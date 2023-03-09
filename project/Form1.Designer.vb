e9c5c598cb5644f1a3f321794059a26c015d5e2b
Hp <Hp@DESKTOP-RHH1I99>
2023-03-09 20:53:03 +0530

    Add project files.

---
+++ project/Form1.Designer.vb
@@ -0,0 +1,331 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.GBoxChoice = New System.Windows.Forms.GroupBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GBoxChoice.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(221, 36)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(823, 84)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Rock  Paper Scissors  Game "
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DarkGray
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.ForeColor = System.Drawing.Color.Purple
+        Me.Button1.Location = New System.Drawing.Point(139, 199)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(227, 73)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "PLAYER"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DarkGray
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.ForeColor = System.Drawing.Color.Purple
+        Me.Button2.Location = New System.Drawing.Point(1126, 199)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(302, 73)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "COMPUTER"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(16, 336)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(481, 286)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(1126, 336)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(481, 286)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'GBoxChoice
+        '
+        Me.GBoxChoice.Controls.Add(Me.Button5)
+        Me.GBoxChoice.Controls.Add(Me.Button4)
+        Me.GBoxChoice.Controls.Add(Me.Button3)
+        Me.GBoxChoice.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.GBoxChoice.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.GBoxChoice.Location = New System.Drawing.Point(38, 704)
+        Me.GBoxChoice.Margin = New System.Windows.Forms.Padding(4)
+        Me.GBoxChoice.Name = "GBoxChoice"
+        Me.GBoxChoice.Padding = New System.Windows.Forms.Padding(4)
+        Me.GBoxChoice.Size = New System.Drawing.Size(459, 290)
+        Me.GBoxChoice.TabIndex = 5
+        Me.GBoxChoice.TabStop = False
+        Me.GBoxChoice.Text = "Click your choice "
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button5.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button5.Location = New System.Drawing.Point(129, 213)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(175, 49)
+        Me.Button5.TabIndex = 2
+        Me.Button5.Text = "SCISSORS"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button4.Location = New System.Drawing.Point(129, 140)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(175, 48)
+        Me.Button4.TabIndex = 1
+        Me.Button4.Text = "PAPER"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.Location = New System.Drawing.Point(129, 66)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(175, 48)
+        Me.Button3.TabIndex = 0
+        Me.Button3.Text = "ROCK"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.GroupBox1.Location = New System.Drawing.Point(1092, 704)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Size = New System.Drawing.Size(505, 290)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Player Score "
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.ForeColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(111, 217)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(48, 28)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "TIES"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(111, 140)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(78, 28)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "LOSSES"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(111, 62)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 28)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "WINS"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.DimGray
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(309, 217)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(23, 28)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.DimGray
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(309, 140)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(23, 28)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.DimGray
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(309, 66)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(23, 28)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "0"
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button6.Location = New System.Drawing.Point(660, 755)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(264, 63)
+        Me.Button6.TabIndex = 7
+        Me.Button6.Text = "PLAY AGAIN"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Segoe UI", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button7.Location = New System.Drawing.Point(710, 941)
+        Me.Button7.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(154, 53)
+        Me.Button7.TabIndex = 8
+        Me.Button7.Text = "EXIT "
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button8.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button8.Location = New System.Drawing.Point(660, 381)
+        Me.Button8.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(307, 200)
+        Me.Button8.TabIndex = 9
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 28.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(1613, 1049)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GBoxChoice)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "RPS game "
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GBoxChoice.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents GBoxChoice As GroupBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ project/Form1.vb
@@ -0,0 +1,282 @@
+﻿Public Class Form1
+
+
+    Public strPlayerchoice As String = "nochoiceYet"
+    Public strComputerchoice As String = "nochoiceYet"
+
+    Public binPlayerWins As Boolean = "False"
+    Public binComputerWins As Boolean = "False"
+    Public intNbrOfPlayerWins As Integer = 0
+    Public intNbrOfPlayerLosses As Integer = 0
+    Public intNbrOfPlayerTies As Integer = 0S
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label2.Text = "0"
+        Label3.Text = "0"
+        Label4.Text = "0"
+
+        PictureBox1.Image = My.Resources.ResourceManager.GetObject("Playerjpg")
+        If PictureBox1.Image Is Nothing Then
+            MessageBox.Show("RPS player image not found in program resources library")
+        End If
+        PictureBox2.Image = My.Resources.ResourceManager.GetObject("Computerjpg")
+        If PictureBox2.Image Is Nothing Then
+            MessageBox.Show("RPS player image not found in program resources library")
+        End If
+        Button8.ForeColor = Color.DarkTurquoise
+        Button8.FlatStyle = FlatStyle.Standard
+        Button8.TextAlign = ContentAlignment.MiddleCenter
+        Button8.Text = "Play now " & vbCrLf & "And Win!"
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        strPlayerchoice = "Rock"
+
+        PictureBox1.Image = My.Resources.ResourceManager.GetObject("Rockplayerjpg")
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        If PictureBox1.Image Is Nothing Then
+            MessageBox.Show("Player rock image not found in program resource library ")
+        End If
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button3.Enabled = False
+        makerandomcoputerchoice()
+        CheckForWinner()
+        DisplayGameResult()
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        strPlayerchoice = "Paper"
+
+        PictureBox1.Image = My.Resources.ResourceManager.GetObject("Paperplayerjpg")
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        If PictureBox1.Image Is Nothing Then
+            MessageBox.Show("Player rock image not found in program resource library ")
+        End If
+        Button3.Enabled = False
+        Button5.Enabled = False
+        Button4.Enabled = False
+        makerandomcoputerchoice()
+        CheckForWinner()
+        DisplayGameResult()
+
+
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        strPlayerchoice = "Scissor"
+
+        PictureBox1.Image = My.Resources.ResourceManager.GetObject("Scissorplayerjpg")
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        If PictureBox1.Image Is Nothing Then
+            MessageBox.Show("Player rock image not found in program resource library ")
+        End If
+        Button4.Enabled = False
+        Button3.Enabled = False
+        Button5.Enabled = False
+        makerandomcoputerchoice()
+        CheckForWinner()
+        DisplayGameResult()
+
+
+    End Sub
+    Public Sub makerandomcoputerchoice()
+        Randomize()
+        Dim intcomputerRandomchoice As Integer = Int((3 * Rnd()) + 1)
+        If intcomputerRandomchoice = 1 Then
+            strComputerchoice = "Rock"
+        ElseIf intcomputerRandomchoice = 2 Then
+            strComputerchoice = "Paper"
+        ElseIf intcomputerRandomchoice = 3 Then
+            strComputerchoice = "Scissor"
+        Else
+            MessageBox.Show("Error in computer choice random number generator ")
+        End If
+        If strComputerchoice = "Rock" Then
+            PictureBox2.Image = My.Resources.ResourceManager.GetObject("Rockcomputerjpg")
+            PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        End If
+        If strComputerchoice = "Paper" Then
+            PictureBox2.Image = My.Resources.ResourceManager.GetObject("Papercomputerjpg")
+            PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        End If
+        If strComputerchoice = "Scissor" Then
+            PictureBox2.Image = My.Resources.ResourceManager.GetObject("Scissorcomputerjpg")
+            PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        End If
+    End Sub
+    Public Sub CheckForWinner()
+        Select Case strPlayerchoice
+            Case "Rock"
+                CheckRockVersusComputerSelection()
+            Case "Paper"
+                CheckPaperVersusComputerSelection()
+            Case "Scissor"
+                CheckScissorVersusComputerSelection()
+            Case Else
+                MessageBox.Show("Error:Player has not selected Rock Paper or Scissors")
+
+        End Select
+    End Sub
+    Public Sub CheckRockVersusComputerSelection()
+        If strComputerchoice = "Rock" Then
+            binComputerWins = "True"
+            binPlayerWins = "True"
+
+        ElseIf strComputerchoice = "Paper" Then
+            binComputerWins = "True"
+
+        ElseIf strComputerchoice = "Scissor" Then
+            binPlayerWins = "True"
+
+        Else
+            MessageBox.Show("Check Winner Error: Computer has not selected rock paper or scissor ")
+        End If
+    End Sub
+    Public Sub CheckPaperVersusComputerSelection()
+        If strComputerchoice = "Rock" Then
+            binPlayerWins = "True"
+
+        ElseIf strComputerchoice = "Paper" Then
+            binComputerWins = "True"
+            binPlayerWins = "True"
+
+
+        ElseIf strComputerchoice = "Scissor" Then
+            binComputerWins = "True"
+
+        Else
+            MessageBox.Show("Check Winner Error: Computer has not selected rock paper or scissor ")
+        End If
+    End Sub
+    Public Sub CheckScissorVersusComputerSelection()
+        If strComputerchoice = "Rock" Then
+            binComputerWins = "True"
+
+        ElseIf strComputerchoice = "Paper" Then
+            binPlayerWins = "True"
+
+        ElseIf strComputerchoice = "Scissor" Then
+            binPlayerWins = "True"
+            binComputerWins = "True"
+
+
+        Else
+            MessageBox.Show("Check Winner Error: Computer has not selected rock paper or scissor ")
+        End If
+    End Sub
+    Public Sub DisplayGameResult()
+        If strPlayerchoice = "Rock" And binPlayerWins = "True" And binComputerWins = "false" Then
+            intNbrOfPlayerWins = intNbrOfPlayerWins + 1
+            Label2.Text = CStr(intNbrOfPlayerWins)
+            Button8.ForeColor = Color.DarkTurquoise
+            Button8.FlatStyle = FlatStyle.Standard
+            Button8.TextAlign = ContentAlignment.MiddleCenter
+            Button8.Text = "Player " & vbCrLf & "Wins!"
+            PictureBox1.Image = My.Resources.ResourceManager.GetObject("GIF_PLAYER_ROCK_WINS")
+        End If
+        If strPlayerchoice = "Paper" And binPlayerWins = "True" And binComputerWins = "false" Then
+            intNbrOfPlayerWins = intNbrOfPlayerWins + 1
+            Label2.Text = CStr(intNbrOfPlayerWins)
+            Button8.ForeColor = Color.DarkTurquoise
+            Button8.FlatStyle = FlatStyle.Standard
+            Button8.TextAlign = ContentAlignment.MiddleCenter
+            Button8.Text = "Player " & vbCrLf & "Wins!"
+            PictureBox1.Image = My.Resources.ResourceManager.GetObject("GIF_PLAYER_PAPER_WINS")
+        End If
+        If strPlayerchoice = "Scissor" And binPlayerWins = "True" And binComputerWins = "false" Then
+            intNbrOfPlayerWins = intNbrOfPlayerWins + 1
+            Label2.Text = CStr(intNbrOfPlayerWins)
+            Button8.ForeColor = Color.DarkTurquoise
+            Button8.FlatStyle = FlatStyle.Standard
+            Button8.TextAlign = ContentAlignment.MiddleCenter
+            Button8.Text = "Player " & vbCrLf & "Wins!"
+            PictureBox1.Image = My.Resources.ResourceManager.GetObject("GIF_PLAYER_SCISSOR_WINS")
+        End If
+        If strComputerchoice = "Rock" And binComputerWins = "true" And binPlayerWins = "false" Then
+            intNbrOfPlayerLosses = intNbrOfPlayerLosses + 1
+            Label3.Text = CStr(intNbrOfPlayerLosses)
+            Button8.ForeColor = Color.Red
+            Button8.FlatStyle = FlatStyle.Standard
+            Button8.TextAlign = ContentAlignment.MiddleCenter
+            Button8.Text = "Computer " & vbCrLf & "Wins!"
+            PictureBox1.Image = My.Resources.ResourceManager.GetObject("GIF_COMPUTER_ROCK_WINS")
+        End If
+        If strComputerchoice = "Paper" And binComputerWins = "true" And binPlayerWins = "false" Then
+            intNbrOfPlayerLosses = intNbrOfPlayerLosses + 1
+            Label3.Text = CStr(intNbrOfPlayerLosses)
+            Button8.ForeColor = Color.Red
+            Button8.FlatStyle = FlatStyle.Standard
+            Button8.TextAlign = ContentAlignment.MiddleCenter
+            Button8.Text = "Computer  " & vbCrLf & "Wins!"
+            PictureBox1.Image = My.Resources.ResourceManager.GetObject("GIF_COMPUTER_PAPER_WINS")
+        End If
+        If strComputerchoice = "Scissor" And binComputerWins = "true" And binPlayerWins = "false" Then
+            intNbrOfPlayerLosses = intNbrOfPlayerLosses + 1
+            Label3.Text = CStr(intNbrOfPlayerLosses)
+            Button8.ForeColor = Color.Red
+            Button8.FlatStyle = FlatStyle.Standard
+            Button8.TextAlign = ContentAlignment.MiddleCenter
+            Button8.Text = "Computer " & vbCrLf & "Wins"
+            PictureBox1.Image = My.Resources.ResourceManager.GetObject("GIF_COMPUTER_SCISSOR_WINS")
+        End If
+        If strComputerchoice = "Scissor" And strPlayerchoice = "Scissor" Then
+            intNbrOfPlayerTies = intNbrOfPlayerTies + 1
+            Label4.Text = CStr(intNbrOfPlayerTies)
+            Button8.ForeColor = Color.Yellow
+            Button8.FlatStyle = FlatStyle.Standard
+            Button8.TextAlign = ContentAlignment.MiddleCenter
+            Button8.Text = "It is a  " & vbCrLf & "Tie game "
+        End If
+        If strComputerchoice = "Rock" And strPlayerchoice = "Rock" Then
+            intNbrOfPlayerTies = intNbrOfPlayerTies + 1
+            Label4.Text = CStr(intNbrOfPlayerTies)
+            Button8.ForeColor = Color.Yellow
+            Button8.FlatStyle = FlatStyle.Standard
+            Button8.TextAlign = ContentAlignment.MiddleCenter
+            Button8.Text = "It is a  " & vbCrLf & "Tie game "
+        End If
+        If strComputerchoice = "Paper" And strPlayerchoice = "Paper" Then
+            intNbrOfPlayerTies = intNbrOfPlayerTies + 1
+            Label4.Text = CStr(intNbrOfPlayerTies)
+            Button8.ForeColor = Color.Yellow
+            Button8.FlatStyle = FlatStyle.Standard
+            Button8.TextAlign = ContentAlignment.MiddleCenter
+            Button8.Text = "It is a  " & vbCrLf & "Tie game "
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim confirm_msg As Integer
+        confirm_msg = MessageBox.Show("Are you sure you want to exit?", "Exit amd close",
+        MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+        If confirm_msg = vbYes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+
+        PictureBox1.Image = My.Resources.ResourceManager.GetObject("Playerjpg")
+        If PictureBox1.Image Is Nothing Then
+            MessageBox.Show("RPS player image not found in program resources library")
+        End If
+        PictureBox2.Image = My.Resources.ResourceManager.GetObject("Computerjpg")
+        If PictureBox2.Image Is Nothing Then
+            MessageBox.Show("RPS player image not found in program resources library")
+        End If
+        Button8.ForeColor = Color.DarkTurquoise
+        Button8.FlatStyle = FlatStyle.Standard
+        Button8.TextAlign = ContentAlignment.MiddleCenter
+        Button8.Text = "Play now " & vbCrLf & "And Win!"
+        Button4.Enabled = True
+        Button5.Enabled = True
+        Button3.Enabled = True
+    End Sub
+End Class
